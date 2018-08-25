a26554528e5475a09e4e5506ebdf52d906c281db
martinpozza <martinpozza@hotmail.com>
2018-08-25 14:49:20 -0300

    Creación de modelos

--- nfl/models.py
+++ nfl/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Equipo(models.Model):
+
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return(self.nombre)
+
+
+class Fixture(models.Model):
+
+    fecha=models.IntegerField
+    local=models.ForeignKey(Equipo,on_delete=models.CASCADE)
+    visitante=models.ForeignKey(Equipo,on_delete=models.CASCADE,related_name='relacion1')
+    gana_local=models.FloatField        #campo para guardar la probabilidad de que gane el local
+    gana_visitante = models.FloatField  #campo para guardar la probabilidad de que gane el visitante
+
+    def __str__(self):
+        return '{} {} {} {} {}'.format(self.fecha,self.local,self.gana_local,self.visitante,self.gana_visitante)
